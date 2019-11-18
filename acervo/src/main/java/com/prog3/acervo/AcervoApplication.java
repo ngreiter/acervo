89a829015c89c3e557da03952f15902e198d1ce3
Nathan Reiter <nathan.reiter@amcom.com.br>
2019-11-18 20:05:19 -0300

    Estrutura inicial Spring Boot

---
+++ acervo/src/main/java/com/prog3/acervo/AcervoApplication.java
@@ -0,0 +1,13 @@
+package com.prog3.acervo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoApplication.class, args);
+	}
+
+}
---
+++ acervo/src/test/java/com/prog3/acervo/AcervoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog3.acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
